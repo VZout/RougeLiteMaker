9f4d465311839c9dbda5a9a072aa8dfb69eae755
Viktor Zoutman <bideblade1@hotmail.com>
2015-11-24 14:53:34 +0100

    Updated system popup

--- src/system_popup.cpp
+++ src/system_popup.cpp
@@ -1,4 +1,10 @@
-#include "system_popup.h"
+#include "systempopup.h"
+
+#include <iostream>
+
+#if defined(__WIN32__) || defined(__WIN64__)
+	#include <windows.h>
+#endif
 
 namespace rlm {
     SystemPopup::SystemPopup() {
@@ -9,10 +15,31 @@ namespace rlm {
 
     }
 
-    void SystemPopup::Create(const char* string) {
+    void SystemPopup::Create(const char* string, SystemPopupType type) {
 
         #if defined(__WIN32__) || defined(__WIN64__)
-            //TODO windows based popup.
+
+			std::string temp = string;
+			std::string message = temp + "\n\nWould you like to quit?";
+
+			switch(type) {
+			case WARNING:
+	    		MessageBox(0, string, "Rouge Lite Maker", MB_OK | MB_ICONWARNING);
+				break;
+
+			case MESSAGE:
+	    		MessageBox(0, string, "Rouge Lite Maker", MB_OK | MB_ICONINFORMATION);
+				break;
+
+			case NONFATAL_ERROR:
+	    		MessageBox(0, message.c_str(), "Rouge Lite Maker", MB_YESNO | MB_ICONERROR);
+				break;
+
+			case FATAL_ERROR:
+	    		MessageBox(0, string, "Rouge Lite Maker", MB_OK | MB_ICONERROR);
+				break;
+
+			}
         #elif __linux__ || __unix__
             std::string str = string;
             std::string prompt = "/usr/bin/notify-send RLM \"" + str + "\" --icon=dialog-error";
