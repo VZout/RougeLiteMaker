b684db7376e2cd04fdf98dda064c96bdc7e58e37
Viktor Zoutman <bideblade1@hotmail.com>
2015-11-24 13:43:23 +0100

    Added windows popup support

--- src/system_popup.cpp
+++ src/system_popup.cpp
@@ -1,5 +1,9 @@
 #include "system_popup.h"
 
+#if defined(__WIN32__) || defined(__WIN64__)
+	#include <windows.h>
+#endif
+
 namespace rlm {
     SystemPopup::SystemPopup() {
 
@@ -9,10 +13,28 @@ namespace rlm {
 
     }
 
-    void SystemPopup::Create(const char* string) {
+    void SystemPopup::Create(const char* string, SystemPopupType type) {
 
         #if defined(__WIN32__) || defined(__WIN64__)
-            //TODO windows based popup.
+
+			switch(type) {
+			case WARNING:
+	    		MessageBox(0, "A problem has occured", "Rouge Lite Maker", MB_OK | MB_ICONWARNING);
+				break;
+
+			case MESSAGE:
+	    		MessageBox(0, "A problem has occured.", "Rouge Lite Maker", MB_OK | MB_ICONINFORMATION);
+				break;
+
+			case NONFATAL_ERROR:
+	    		MessageBox(0, "A problem has occured.\n\nWould you like to quit?", "Rouge Lite Maker", MB_YESNO | MB_ICONERROR);
+				break;
+
+			case FATAL_ERROR:
+	    		MessageBox(0, "A problem has occured.\n\nWould you like to quit?", "Rouge Lite Maker", MB_YESNO | MB_ICONERROR);
+				break;
+
+			}
         #elif __linux__ || __unix__
             std::string str = string;
             std::string prompt = "/usr/bin/notify-send RLM \"" + str + "\" --icon=dialog-error";
