29bcd5aebc1e8e9d518a89e3c6c5a28c62f6515e
Viktor Zoutman <bideblade1@hotmail.com>
2015-11-24 13:42:51 +0100

    Added windows popup support

--- src/system_popup.h
+++ src/system_popup.h
@@ -13,3 +13,26 @@ namespace rlm {
 }
 
 #endif
+#ifndef SYSTEMPOPUP_H_
+#define SYSTEMPOPUP_H_
+
+#include <string>
+
+namespace rlm {
+
+
+    class SystemPopup {
+    public:
+		enum SystemPopupType {
+			MESSAGE,
+			WARNING,
+			NONFATAL_ERROR,
+			FATAL_ERROR,
+		};
+        SystemPopup();
+        ~SystemPopup();
+        void Create(const char* string, SystemPopupType type);
+    };
+}
+
+#endif
