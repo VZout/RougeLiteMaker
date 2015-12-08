c2d6dc05c464a58ef08056408034d49cf61b8766
unknown <bideblade1@hotmail.com>
2015-12-08 13:40:04 +0100

    Added basic collision.

--- src/collider.cpp
+++ src/collider.cpp
@@ -1,11 +1,46 @@
 #include "collider.h"
 
 namespace rlm {
-    Collider() {
-
+    Collider(glm::vec2 pos, glm::vec2 size) {
+        _pos = pos;
+        _size = size;
     }
 
     ~Collider() {
 
+    }
+
+    bool Collider::Overlaps(BoxCollider other) {
+        if(other._pos.x + other._size.x > _pos.x + _size.x &&
+            other._pos.x < _pos.x + _size.x &&
+            other._pos.y + other._size.y > _pos.y &&
+            other._pos.y < _pos.y + _size.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool Collider::Overlaps(glm::vec2 point) {
+        if(point.x > _pos.x &&
+            point.x < _pos.x + _size.x &&
+            point.y > _pos.y &&
+            point.y < _pos.y + _size.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    CollisionSides Collider::GetCollision(BoxCollision other) {
+        return TOP;
+    }
+
+    glm::vec2 GetPos() {
+        return _pos;
+    }
+
+    glm::vec2 GetSize() {
+        return _size;
     }
 }
--- src/collider.h
+++ src/collider.h
@@ -4,12 +4,29 @@
 #include "glm/glm.hpp"
 
 namespace rlm {
+
+    enum CollisionSides {
+            TOP,
+            BOTTOM,
+            LEFT,
+            RIGHT,
+    };
+
     class BoxCollider() {
+    private:
+        glm::vec2 _pos;
+        glm::vec2 _size;
     public:
         Collider(glm::vec2 pos, glm::vec2 size);
         ~Collider();
         glm::vec2 GetPos();
         glm::vec2 GetSize();
+        bool Overlaps(BoxCollider other);
+        bool Overlaps(glm::vec2 point);
+        glm::vec2 GetPos();
+        glm::vec2 GetSize();
+        CollisionSides HasCollision(BoxCollider other);
+
     };
 }
 
